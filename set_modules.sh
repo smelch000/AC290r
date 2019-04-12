fd2e92b9bf1d6d6d747199e561826c1f14ed04f8
Simone Melchionna <simone.melchionna@lexma-tech.com>
2019-04-12 13:38:42 -0400

    fix

--- set_modules.sh
+++ set_modules.sh
@@ -1,1 +1,17 @@
+
+echo '
+
+From bash shell:
+
+1. run the following commands to load the proper modules:
+module purge
 module load gcc/7.1.0-fasrc01 openmpi/3.1.1-fasrc01  cuda/10.0.130-fasrc01
+
+2. set up the following variables:
+
+export MOEBIUS_ROOT=_path_the_MAGIC_folder
+export PYTHONPATH=$PYTHONPATH:$MOEBIUS_ROOT/BACKEND/SHOP:$MOEBIUS_ROOT/BACKEND/SCRIPTS
+'
+
+
+

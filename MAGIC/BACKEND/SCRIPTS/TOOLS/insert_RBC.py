2524ff62f34f8b0f70a3e182251611e66b34ce24
SimoneX MelchionnaX <ac290ru1901@boslogin01.rc.fas.harvard.edu>
2019-04-22 11:37:48 -0400

    added

---
+++ MAGIC/BACKEND/SCRIPTS/TOOLS/insert_RBC.py
@@ -0,0 +1,316 @@
+#!/usr/bin/env python
+
+import os, sys, math
+import random
+from ctypes import *
+from numpy import empty
+import argparse
+
+import TOOLS.mesh as msh
+from TOOLS.wrap_by_wall import *
+import numpy as np
+
+# RBC model parameters
+__VRBC = 2.1 # volume of RBC
+__DMIN_RBC = 1.0    # minimum diameter RBC's
+__DMAX_RBC = 2.0    # maximum diameter RBC's
+
+# __CUTOFF_INSERTION = 1.0 # 2.0 # cutoff for random insertion of particles
+__CUTOFF_INSERTION = 1.2
+__CUTOFF_WALL = 0.5
+__GAMMAT = 0.0001
+__GAMMAR = 0.0001
+
+def generate_cylinder(RADIUS,NX,NY,NZ):
+
+    print 'CYLINDER RADIUS:',RADIUS,'(l.u.) = ',RADIUS * 4.,'(micron)'
+    print 'CYLINDER LENGTH:',NX,'(l.u.) = ',NX * 4.,'(micron)'
+    print 'NX:',NX,'    NY,NZ:',NY,NZ
+
+    MSH = msh.Mesh(nx=NX,ny=NY,nz=NZ)
+
+    RADIUS2 = RADIUS * RADIUS
+
+    itp_f, itp_w, itp_i, itp_o = [], [], [], []
+    ii_f, jj_f, kk_f = [], [], []
+    ii_w, jj_w, kk_w = [], [], []
+
+    #
+    # paint the mesh
+    for k in xrange(1,MSH.nz+1):
+        for j in xrange(1,MSH.ny+1):
+            for i in xrange(1,MSH.nx+1):
+
+                d = (j-MSH.ny/2)**2 + (k-MSH.nz/2)**2
+
+                if d < RADIUS2:
+                    itp_f.append( MSH.i4back(i,j,k) )
+                    ii_f.append(i)
+                    jj_f.append(j)
+                    kk_f.append(k)
+                else:
+                    ii_w.append(i)
+                    jj_w.append(j)
+                    kk_w.append(k)
+
+    MSH.specifyNodes(itp_f,itp_w,itp_i,itp_o)
+
+    # complete by wrapping
+    wrap_by_wall(MSH)
+
+    print 'Nfluid nodes:',MSH.nfluid
+
+    # mesh is complete
+    # print 'writing mesh.xyz'
+    # MSH.writexyz('mesh.xyz')
+
+    MSH.writeMOEBIUSinput('bgkflag')
+    return MSH, ii_f, jj_f, kk_f, ii_w, jj_w, kk_w, itp_f, itp_w, itp_i, itp_o
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-H', '--hematocrit', required=True,  help='hematocrit in percentile (0.0: ~0.50 range)')
+    parser.add_argument('-m', '--mesh', required=False,  help='input mesh file')
+    parser.add_argument('-R', '--radius', required=False,  help='cylinder radius (l.u.)')
+    parser.add_argument('-A', '--alignment', required=True,  help='alignment of system (x, y or z)')
+
+    args = parser.parse_args()
+
+    HCT = float(args.hematocrit)
+    if HCT < 0. or HCT > 0.60:
+        print 'hematrocit level unreasonable:', HCT, '...set in the [0-0.50] range and rerun'
+        sys.exit(1)
+
+    alignment = args.alignment
+    if alignment not in ['x','y','z']:
+        print 'alignment can only be x,y or z'
+        sys.exit(1)
+
+    if args.mesh != None:
+
+        MSH = Mesh()
+        MSH.loadMOEBIUSinput(args.mesh+'.hdr', args.mesh+'.dat')
+
+    else:
+        if args.radius == None:
+            print 'For the test case, insert cylinder radius'
+            sys.exit(1)
+
+        RADIUS = float(args.radius)
+        NX = int(RADIUS * 6)
+        NY = int(2*RADIUS) + 10
+        NZ = int(2*RADIUS) + 10
+
+        # generate mesh files for a test cylinder
+        MSH, ii_f, jj_f, kk_f, ii_w, jj_w, kk_w, itp_f, itp_w, itp_i, itp_o = generate_cylinder(RADIUS,NX,NY,NZ)
+
+    __NRBC = int(HCT  * MSH.nfluid  / __VRBC)
+
+    # Spheres model parameter
+    __NSPH = 0
+    __D_SPH    = 1.0    # diameter spheres
+
+    print 'Hematocrit:',HCT
+    print 'NRBC:',__NRBC, 'Volume of RBC:', __VRBC,'l.u.'
+    print 'NSPH = ',__NSPH
+
+    # write atom.inp
+    a = open('atom.inp','w')
+    print >> a, \
+"""
+MODEL
+# title
+molecular types    3
+_Ellipsoids
+nummols    %d
+atoms    1
+E  1. 0.  1  0  .5 1. 1. 0   ! name, mass, chge, rept, frzt, inertia x/y/z, frzr
+hydro
+.false. 0.04   1.6  2.0   2.0  0.0      ! passive scalar, tumbling coeff, visc_enhancer, csi, oblate, smooth
+%f    %f  ! gammas
+finish
+_Spheroids
+nummols    %d
+atoms    1
+S  %f 0.  1  0  1.0 1.0 1.0 0   ! name, mass, chge, rept, frzt, inertia x/y/z, frzr
+hydro
+.false. 0.04   1.6  2.0   2.0  0.0      ! passive scalar, tumbling coeff, visc_enhancer, csi, oblate, smooth
+%f    %f  ! gammas
+finish
+Wall
+nummols    1
+atoms    1
+W
+finish
+vdw    6
+E E gb    1.e-4   1.0  2.0  2.0    0.0
+E S gb    1.e-4   %f %f   %f   0.0
+S S gb    1.e-4   %f %f %f    0.0
+E W lj    1.e-4   1.0  0.0  0.0    0.0
+S W lj    1.e-4   %f  0.0  0.0    0.0
+W W lj    1.e-4   1.0  0.0  0.0    0.0
+CLOSEMODEL
+""" % (__NRBC,__GAMMAT,__GAMMAR,__NSPH,(8*(__D_SPH/2)**3),__GAMMAR,__GAMMAT, \
+      (__D_SPH + __DMIN_RBC)/2., (__D_SPH + __DMAX_RBC)/2., (__D_SPH + __DMAX_RBC)/2., \
+      __D_SPH, __D_SPH, __D_SPH, \
+      __D_SPH)
+
+    print >> a, "CONFIG"
+    print >> a, __NRBC + __NSPH
+
+    """
+    i4rand = random.sample(itp_f, __NRBC + __NSPH)
+    print 'atom position uniqueness:',len(i4rand) == len(set(i4rand))
+    atompos = []
+    for i4 in i4rand:
+        atompos.append( MSH.ijk(i4) )
+    print >> a, "CONFIG"
+    print >> a, len(atompos)
+    V = 0.001
+    for x,y,z in atompos:
+        # print >> a, x,y,z,0.,0.,0.
+        print >> a, x,y,z,random.uniform(-V,V),random.uniform(-V,V),random.uniform(-V,V)
+    """
+
+    # now start insersion of RBC (+ SPH)
+
+    def dist2(a,b):
+
+        def NINT(f):
+            return int(f + 0.5)
+
+        dx = a[0] - b[0]; dy = a[1] - b[1]; dz = a[2] - b[2]
+        dx = dx - NINT(dx/NX)*NX
+        # dy = dy - NINT(dy/BOX)*BOX
+        # dz = dz - NINT(dz/BOX)*BOX
+        return dx**2 + dy**2 + dz**2
+
+    cut2 = __CUTOFF_INSERTION**2
+
+    p = open('RBC.xyz','w')
+    print >> p, __NRBC
+    print >> p
+
+    USE_SO_LIBRARY = True
+
+    if USE_SO_LIBRARY:
+
+
+        fnr = os.path.join(os.getenv("MOEBIUS_ROOT"), 'BACKEND/SCRIPTS/TOOLS/insert_RBC_part')
+        os.system('gfortran -cpp -DLINKCELL -g -shared -fPIC %s.f90 -o %s.so'%(fnr,fnr))
+
+        # LIB = CDLL('./preproc_part.so', RTLD_GLOBAL)
+        LIB = CDLL('%s.so'%fnr, RTLD_GLOBAL)
+
+        # NSZ = MSH.nfluid + MSH.nwall
+
+        ii_f_l = empty(MSH.nfluid, dtype="int64")
+        jj_f_l = empty(MSH.nfluid, dtype="int64")
+        kk_f_l = empty(MSH.nfluid, dtype="int64")
+
+        ii_w_l = empty(MSH.nwall, dtype="int64")
+        jj_w_l = empty(MSH.nwall, dtype="int64")
+        kk_w_l = empty(MSH.nwall, dtype="int64")
+
+        if args.mesh != None:
+            for ifl in range(MSH.nfluid):
+                i4 = MSH.itppp_f[ifl]
+                i,j,k = MSH.ijk(i4)
+                ii_f_l[ifl] = i
+                jj_f_l[ifl] = j
+                kk_f_l[ifl] = k
+
+            for ifl in range(MSH.nwall):
+                i4 = MSH.itppp_w[ifl]
+                i,j,k = MSH.ijk(i4)
+                ii_w_l[ifl] = i
+                jj_w_l[ifl] = j
+                kk_w_l[ifl] = k
+
+        else:
+            ii_f_l = np.copy(ii_f)
+            jj_f_l = np.copy(jj_f)
+            kk_f_l = np.copy(kk_f)
+            ii_w_l = np.copy(ii_w)
+            jj_w_l = np.copy(jj_w)
+            kk_w_l = np.copy(kk_w)
+
+
+        xo = empty(MSH.nfluid, dtype="double")
+        yo = empty(MSH.nfluid, dtype="double")
+        zo = empty(MSH.nfluid, dtype="double")
+
+        if alignment == 'x': ialign = 1
+        if alignment == 'y': ialign = 2
+        if alignment == 'z': ialign = 3
+
+        LIB.preproc_part(c_int(ialign), \
+                         c_int(MSH.nx), c_int(MSH.ny), c_int(MSH.nz), \
+                         c_int(MSH.nfluid), c_int(MSH.nwall), \
+                         c_int(__NRBC), c_int(__NSPH), \
+                         c_float(__CUTOFF_INSERTION), c_float(__CUTOFF_WALL), \
+                         ii_f_l.ctypes.data_as(POINTER(c_int64)), \
+                         jj_f_l.ctypes.data_as(POINTER(c_int64)), \
+                         kk_f_l.ctypes.data_as(POINTER(c_int64)), \
+                         ii_w_l.ctypes.data_as(POINTER(c_int64)), \
+                         jj_w_l.ctypes.data_as(POINTER(c_int64)), \
+                         kk_w_l.ctypes.data_as(POINTER(c_int64)), \
+                         xo.ctypes.data_as(POINTER(c_double)), \
+                         yo.ctypes.data_as(POINTER(c_double)), \
+                         zo.ctypes.data_as(POINTER(c_double)))
+
+        for i in xrange(__NRBC + __NSPH):
+            print >> a, xo[i],yo[i],zo[i],0.,0.,0.,i+1
+            print >> p, 'C', xo[i],yo[i],zo[i]
+
+    else:
+
+        rro = []
+        while True:
+            i4 = random.sample(itp_f, 1)
+            rr = MSH.ijk(i4[0])
+
+            tooclose = False
+
+            d = math.sqrt( (rr[1]-MSH.ny/2)**2 + (rr[2]-MSH.nz/2)**2 )
+            # if d > RADIUS - __CUTOFF_INSERTION:
+            if d > RADIUS - 0.50:
+                tooclose = True
+
+            if not tooclose:
+                for ro in rro:
+                    if dist2(rr,ro) < cut2:
+                        tooclose = True
+                        break
+
+            if not tooclose:
+                # dd = 1.e5
+                # for ro in rro:
+                #     dd =  min(dd,math.sqrt(dist2(rr,ro)))
+                # print 'accepted ....mindist : ', dd
+
+                rro.append(rr)
+                if len(rro)%1000 == 0: print len(rro),'/',__NRBC + __NSPH
+                if len(rro) >= __NRBC + __NSPH: 
+                    break
+
+        for i in xrange(__NRBC + __NSPH):
+            r = rro[i]
+            print >> a, r[0],r[1],r[2],0.,0.,0.,i+1
+            print >> p, 'C', r[0],r[1],r[2]
+
+    for i in xrange(__NRBC + __NSPH):
+        if i%3==0:
+            print >> a, '1. 0. 0. 0. 0. 0.',i+1
+        elif i%3==1:
+            print >> a, '0. 1. 0. 0. 0. 0.',i+1
+        elif i%3==2:
+            print >> a, '0. 0. 1. 0. 0. 0.',i+1
+
+    print >> a, 'CLOSECONFIG'
+
+    a.close()
+
---
+++ MAGIC/BACKEND/SCRIPTS/TOOLS/insert_RBC_part.f90
@@ -0,0 +1,247 @@
+MODULE all_mod
+
+USE ISO_C_BINDING
+
+IMPLICIT NONE
+
+INTEGER :: nx,ny,nz
+INTEGER :: nx2 = 0, nxy2 = 0
+
+CONTAINS
+
+INTEGER FUNCTION ijk(idir, i4)
+INTEGER :: idir,iy,iz
+INTEGER(C_LONG) :: i4
+
+      iz = i4/INT(nxy2)
+      IF(idir==3) THEN
+         ijk = INT(iz)
+         RETURN
+      ENDIF
+
+      iy = (i4 - iz*INT(nxy2))/INT(nx2)
+      IF(idir==2) THEN
+         ijk = INT(iy)
+         RETURN
+      ENDIF
+
+      ijk = INT(i4 - iz*INT(nxy2) - iy*INT(nx2))
+
+
+ENDFUNCTION
+
+FUNCTION random_ijk(itp, n) RESULT(out)
+INTEGER(C_LONG) :: itp(0:)
+INTEGER :: n
+INTEGER :: nrnd
+REAL :: out(3)
+REAL :: rnd
+INTEGER(C_LONG) :: i4
+INTEGER :: i,j,k
+
+    CALL RANDOM_NUMBER(rnd)
+    nrnd = INT(rnd * SIZE(itp))
+    i4 = itp(nrnd)
+    i = ijk(1,i4)
+    j = ijk(2,i4)
+    k = ijk(3,i4)
+    print *,'nrnd:',nrnd,i,j,k
+    out = [i,j,k]
+
+ENDFUNCTION
+
+REAL FUNCTION dist2(r1,r2)
+    REAL :: r1(:), r2(:)
+    REAL :: dx,dy,dz
+
+    dx = r1(1) - r2(1)
+    dy = r1(2) - r2(2)
+    dz = r1(3) - r2(3)
+
+    dx = dx - NINT(dx/NX)*NX
+
+    dist2 = dx**2 + dy**2 + dz**2
+
+ENDFUNCTION
+
+ENDMODULE
+
+SUBROUTINE preproc_part(ialign, &
+                        nx_l, ny_l, nz_l, &
+                        nfl, nwl, &
+                        nrbc, nsph, &
+                        cutoff_insertion, cutoff_wall, &
+                        ii_f,jj_f,kk_f, &
+                        ii_w,jj_w,kk_w, &
+                        xo, yo, zo) BIND(C,NAME='preproc_part')
+
+USE all_mod
+
+IMPLICIT NONE
+
+INTEGER(C_INT),INTENT(in),VALUE :: ialign
+INTEGER(C_INT),INTENT(in),VALUE :: nx_l,ny_l,nz_l,nfl,nwl,nrbc,nsph
+REAL(C_FLOAT),INTENT(in),VALUE :: cutoff_insertion, cutoff_wall
+INTEGER(C_LONG),DIMENSION(0:nfl-1) :: ii_f, jj_f, kk_f
+INTEGER(C_LONG),DIMENSION(0:nfl-1) :: ii_w, jj_w, kk_w
+! REAL(C_FLOAT),DIMENSION(1:nfl) :: xo, yo, zo
+REAL(C_DOUBLE),DIMENSION(1:nfl) :: xo, yo, zo
+
+! REAL :: cy,cz
+REAL :: d2
+LOGICAL :: tooclose
+REAL :: rr(3), ro(3) ! , rro(3,nx_l*ny_l*nz_l)
+INTEGER :: nold,i,j,k,nrnd
+REAL :: cut2,cutw2
+REAL :: rnd
+INTEGER :: ib
+INTEGER,ALLOCATABLE,SAVE :: ncx(:),indx(:,:)
+
+    CALL RANDOM_SEED()
+
+    nx = nx_l
+    ny = ny_l
+    nz = nz_l
+    nx2 = nx + 2
+    nxy2 = (nx + 2) * (ny + 2)
+
+#if defined(LINKCELL)
+    print *
+    IF     (ialign==1) THEN ! x-axis
+        print *,'x-wise alignment'
+        ALLOCATE(ncx(1:nx), indx(1:nx, 1:nfl))
+
+    ELSE IF(ialign==2) THEN ! y-axis
+        print *,'y-wise alignment'
+        ALLOCATE(ncx(1:ny), indx(1:ny, 1:nfl))
+
+    ELSE IF(ialign==3) THEN ! z-axis
+        print *,'z-wise alignment'
+        ALLOCATE(ncx(1:nz), indx(1:nz, 1:nfl))
+
+    ENDIF
+    ncx(:) = 0
+#endif
+
+    print *,'...Fast particle placement...'
+    !print *,'VALS:',nx_l,ny_l,nz_l,nfl,nwl,nrbc,nsph
+    print *,'nx,ny,nz:',nx,ny,nz
+    print *,'part-part cutoff:',cutoff_insertion,'part-wall cutoff:',cutoff_wall
+    !print *,ii_f(0:10)
+    !print *,jj_f(0:10)
+    !print *,kk_f(0:10)
+
+    cut2 = cutoff_insertion**2
+    cutw2 = cutoff_wall**2
+
+    nold = 0
+    ! cy = NY/2.; cz = NZ/2.
+    DO WHILE (.true.)
+
+        CALL RANDOM_NUMBER(rnd)
+
+        ! nrnd = INT(rnd * SIZE(ii_f))
+        nrnd = INT(rnd * nfl)
+
+        rr = [ii_f(nrnd), jj_f(nrnd), kk_f(nrnd)]
+        ! print *,'rr:',rr
+
+        tooclose = .false.
+
+#if 0
+        rw2 = radius - SQRT(cutw)
+        d2 = (rr(2) - cy)**2 + (rr(3) - cz)**2
+        tooclose = (d2 > rw2) 
+#else
+        ! check if the RBC is too close to the wall
+        DO i = 1, SIZE(ii_w)
+            ro(1) = ii_w(i)
+            ro(2) = jj_w(i)
+            ro(3) = kk_w(i)
+            IF ( dist2(ro, rr) < cutw2 ) THEN
+                tooclose = .true.
+                EXIT
+            ENDIF
+        ENDDO
+#endif
+
+        ! check if the RBC is too close to a previously inserted RBC
+        IF (.NOT. tooclose) THEN
+
+#if !defined(LINKCELL)
+            DO i = 1, nold
+                IF ( dist2(ro, rr) < cut2 ) THEN
+                    tooclose = .true.
+                    EXIT
+                ENDIF
+            ENDDO
+#else
+            ! little link cell
+            DO i = ii_f(nrnd) - 1, ii_f(nrnd) + 1 
+              ib = i
+              IF      (ialign == 1) THEN
+                  IF ( ib < 1 )  ib = ib + nx
+                  IF ( ib > nx ) ib = ib - nx
+              ELSE IF (ialign == 2) THEN
+                  IF ( ib < 1 )  ib = ib + ny
+                  IF ( ib > ny ) ib = ib - ny
+              ELSE IF (ialign == 3) THEN
+                  IF ( ib < 1 )  ib = ib + nz
+                  IF ( ib > nz ) ib = ib - nz
+              ENDIF
+
+              DO j = 1, ncx(ib)
+                k = indx(ib,j)
+                ro = [xo(k), yo(k), zo(k)]
+                IF ( dist2(ro, rr) < cut2 ) THEN
+                    tooclose = .true.
+                    EXIT
+                ENDIF
+              ENDDO
+            ENDDO
+#endif
+        ENDIF
+    
+        IF (.NOT. tooclose) THEN
+            IF(MOD(nold,1000)==0) print *,'accept nold:',nold,'/',nrbc + nsph, &
+                                           INT(nold*100./(nrbc + nsph)),'%'
+
+            nold = nold + 1
+            ! rro(:,nold) = rr(:)
+            xo(nold) = rr(1)
+            yo(nold) = rr(2)
+            zo(nold) = rr(3)
+
+#if defined(LINKCELL)
+            ib = rr(ialign)
+            ncx(ib) = ncx(ib) + 1
+            indx(ib,ncx(ib)) = nold
+#endif
+
+            IF (nold >= nrbc + nsph) EXIT
+        ENDIF
+
+    ENDDO
+
+    ! xo(1:nold) = rro(1,1:nold)
+    ! yo(1:nold) = rro(2,1:nold)
+    ! zo(1:nold) = rro(3,1:nold)
+
+    !DO i=1, 10 ! SIZE(xo)
+    !    print *,'xo_F:',i,xo(i)
+    !ENDDO
+
+#if 0
+    ! ctypes does not like to open files...crash
+    OPEN(UNIT=1000, STATUS='unknown', file='FettaSalame.xyz')
+    WRITE(1000,*) nold
+    WRITE(1000,*) 
+    DO i=1,nold
+        print *,i,'...write'
+        WRITE(1000,*) 'O', xo(i), yo(i), zo(i)
+    ENDDO
+    CLOSE(1000)
+    STOP
+#endif
+
+ENDSUBROUTINE
